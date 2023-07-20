eac447f4522118939ec3eccf2c7d5b478ebebd0f
danger_videograph <nikolastartsev@gmail.com>
2023-07-20 19:57:59 +0300

    commit

--- main.py
+++ main.py
@@ -1,103 +1,114 @@
-import socket
-from urllib.parse import urlparse, parse_qs
-from datetime import datetime
 import json
+import mimetypes
 import os
+import socket
+import urllib.parse
+from datetime import datetime
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from pathlib import Path
+from threading import Thread
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FRONT_DIR = os.path.join(BASE_DIR, "front-init")
+STORAGE_DIR = os.path.join(FRONT_DIR, "storage")
+FILE_STORAGE = os.path.join(STORAGE_DIR, "data.json")
 
-def handle_request(client_socket, request):
-    request_lines = request.split("\r\n")
-    request_line = request_lines[0]
+HTTP_IP = '0.0.0.0'
+HTTP_PORT = 3000
+SOCKET_IP = '127.0.0.1'
+SOCKET_PORT = 5000
 
-    method, path, protocol = request_line.split(" ")
-    path = urlparse(path)
 
-    if method == "GET":
-        if path.path == "/":
-            with open(os.path.join(BASE_DIR, "front-init", "index.html"), "r") as file:
-                html_content = file.read()
-            response = "HTTP/1.1 200 OK\r\n"
-            response += "Content-type: text/html\r\n"
-            response += "Content-Length: {}\r\n\r\n".format(len(html_content))
-            response += html_content
-
-        elif path.path == "/message.html":
-            with open(os.path.join(BASE_DIR, "front-init", "message.html"), "r") as file:
-                response = "HTTP/1.1 200 OK\r\n"
-                response += "Content-type: text/html\r\n\r\n"
-                response += file.read()
-
+class HttpHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == '/':
+            self.send_html_file('index.html')
+        elif pr_url.path == '/message.html':
+            self.send_html_file('message.html')
         else:
-            with open(os.path.join(BASE_DIR, "front-init", "error.html"), "r") as file:
-                response = "HTTP/1.1 404 Not Found\r\n"
-                response += "Content-type: text/html\r\n\r\n"
-                response += file.read()
+            if Path(FRONT_DIR, pr_url.path[1:]).exists():
+                self.send_static(pr_url.path)
+            else:
+                self.send_html_file('error.html', 404)
 
-    elif method == "POST":
-        content_length = 0
-        for line in request_lines:
-            if line.startswith("Content-Length:"):
-                content_length = int(line.split(":")[1].strip())
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        data = self.rfile.read(content_length)
+        send_data_to_socket(data)
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
 
-        post_data = client_socket.recv(content_length).decode("utf-8")
-        post_params = parse_qs(post_data)
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open(os.path.join(FRONT_DIR, filename), 'rb') as fd:
+            self.wfile.write(fd.read())
 
-        if path.path == "/message.html":
-            username = post_params["username"][0]
-            message = post_params["message"][0]
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-
-            DATA_FILE = os.path.join(BASE_DIR, "front-init", "storage", "data.json")
-
-            message_data = {
-                "username": username,
-                "message": message,
-                "timestamp": timestamp
-            }
-
-            try:
-                with open(DATA_FILE, "r") as file:
-                    messages = json.load(file)
-            except FileNotFoundError:
-                messages = {}
-
-            messages[timestamp] = message_data
-
-            with open(DATA_FILE, "w") as file:
-                json.dump(messages, file, indent=4)
-
-            response = "HTTP/1.1 200 OK\r\n"
-            response += "Content-type: text/html\r\n\r\n"
-            response += "Message received and saved"
-
+    def send_static(self, path):
+        self.send_response(200)
+        mt = mimetypes.guess_type(path)
+        if mt:
+            self.send_header("Content-type", mt[0])
         else:
-            with open(os.path.join(BASE_DIR, "front-init", "error.html"), "r") as file:
-                response = "HTTP/1.1 404 Not Found\r\n"
-                response += "Content-type: text/html\r\n\r\n"
-                response += file.read()
-
-    else:
-        response = "HTTP/1.1 501 Not Implemented\r\n"
-        response += "Content-type: text/html\r\n\r\n"
-        response += "<h1>501 Not Implemented</h1>"
-
-    client_socket.sendall(response.encode("utf-8"))
-    client_socket.close()
+            self.send_header("Content-type", 'text/plain')
+        self.end_headers()
+        with open(os.path.join(FRONT_DIR, path), 'rb') as file:
+            self.wfile.write(file.read())
 
 
-def run_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ("", 3000)
-    server_socket.bind(server_address)
-    server_socket.listen(1)
-    print("Server started on port 3000")
-
-    while True:
-        client_socket, client_address = server_socket.accept()
-        request = client_socket.recv(4096).decode("utf-8")
-        handle_request(client_socket, request)
+def send_data_to_socket(data):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(data, (SOCKET_IP, SOCKET_PORT))
+    sock.close()
 
 
-if __name__ == "__main__":
-    run_server()
+def run_http_server(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = (HTTP_IP, HTTP_PORT)
+    http = server_class(server_address, handler_class)
+    try:
+        http.serve_forever()
+    except KeyboardInterrupt:
+        http.server_close()
+
+
+def run_socket_server(ip=SOCKET_IP, port=SOCKET_PORT):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, port))
+    try:
+        while True:
+            data, _ = sock.recvfrom(1024)
+            save_data_to_json(data)
+    except KeyboardInterrupt:
+        sock.close()
+
+
+def save_data_to_json(data):
+    data_parse = urllib.parse.unquote_plus(data.decode())
+    data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+    try:
+        with open(FILE_STORAGE, 'r') as f:
+            storage = json.load(f)
+    except ValueError:
+        storage = {}
+    storage.update({str(datetime.now()): data_dict})
+    with open(FILE_STORAGE, 'w') as f:
+        json.dump(storage, f)
+
+
+def main():
+    if not os.path.exists(STORAGE_DIR):
+        os.makedirs(STORAGE_DIR)
+    if not os.path.exists(FILE_STORAGE):
+        with open(FILE_STORAGE, 'w') as f:
+            json.dump({}, f)
+    http_server = Thread(target=run_http_server)
+    socket_server = Thread(target=run_socket_server)
+    http_server.start()
+    socket_server.start()
+
+
+if __name__ == '__main__':
+    main()
